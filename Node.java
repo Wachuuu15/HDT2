70b318c74832fe94a4fc1939db8fe71357c03bf8
wachuuu <88355602+Wachuuu15@users.noreply.github.com>
2022-02-15 11:14:48 -0600

    hdt22

---
+++ Node.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+/**
+ * @author moises.alonso
+ *
+ */
+public class Node<T> {
+	
+	private T Value;
+	Node<T> next;
+	
+	public Node() {
+		next = null;
+	}
+	
+	public Node(T Value) {
+		this.Value = Value;
+		next = null;
+	}
+	
+	public T getValue() {
+		return Value;
+	}
+	
+	public void setValue(T value) {
+		Value = value;
+	}
+	
+	public Node<T> getNext() {
+		return next;
+	}
+	
+	public void setNext(Node<T> next) {
+		this.next = next;
+	}
+
+}
